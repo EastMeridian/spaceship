2c7ac14495fa644a74365867a06850cd789e8fcf
gbeillard <114152144+gbeillard@users.noreply.github.com>
2022-11-10 11:19:01 +0100

    fixed message

--- src/adapters/radars/RadarSensorMessage.test.ts
+++ src/adapters/radars/RadarSensorMessage.test.ts
@@ -3,7 +3,7 @@ import { RadarSensorMessage } from "./RadarSensorMessage";
 const TEXT = "An enemy detected";
 const CRYPTED_TEXT = "detceted ymene nA";
 
-describe("RadarSensorAdapter", () => {
+describe("RadarSensorMessage", () => {
   it("should decode a crypted message", () => {
     const message = new RadarSensorMessage();
     const data = message.sendSpecific(TEXT);
